7c4aec36fffd5a652a1aeff39c9606a4fa8c0ea1
Soliev227dierbek <solievdierbek1@gmail.com>
2025-03-11 14:03:13 +0500

    second commit

--- olx/urls.py
+++ olx/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product_detail/<slug:slug>', views.product_detail, name='product_detail_url'),
+    path('search/', views.search, name='search'),
 ]
--- olx/views.py
+++ olx/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.db.models import Q
 
 def index(request):
     products = Product.objects.all()[:3]
@@ -8,4 +9,9 @@ def index(request):
 def product_detail(request, slug):
     product = Product.objects.get(slug__iexact = slug)
     return render(request, 'olx/product_detail.html', {'product':product})
+
+def search(request):
+    query = request.GET.get('search')
+    search_obj = Product.objects.filter(Q(title__iregex = query))
+    return render(request, 'olx/search.html', {'search_obj':search_obj, 'query':query})
 # Create your views here.
